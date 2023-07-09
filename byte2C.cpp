c5f4282075a055f56b692f25603ef2a0749a9763
Peppe <example@example.com>
2023-07-09 23:58:10 +0200

    added hardcode size

--- byte2C.cpp
+++ byte2C.cpp
@@ -11,12 +11,13 @@ if (argc<2) {
 	return -1;
 }
 fi.open(argv[1],std::ios::in);
-fo.open("hardcode.c",std::ios::out);
+fo.open("audiomem.h",std::ios::out);
+
 fo << "uint8_t audio[]={";
 
 uint8_t cur,element=0;
 bool firstelement=true;
-
+uint32_t cnt_by=0;
 while(fi.read((char*)&cur,1)) {
 	if(firstelement) {
 		firstelement=false;
@@ -29,9 +30,12 @@ while(fi.read((char*)&cur,1)) {
 		fo << std::endl;
 		element=0;
 	}
+	cnt_by++;
 }
 
 fo << "};" << std::endl;
+
+fo << "uint32_t asize=" << cnt_by << ";" << std::endl;
 
 return 0;
 }
